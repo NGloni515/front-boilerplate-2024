4bc80707ab4874812fe1a358682c0396268d92c0
Angeloni Cuesta <angelonicuesta@192.168.1.4>
2023-11-18 18:04:07 -0500

    i18n

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,83 +1,92 @@
 import Axios, {
-    AxiosError,
-    AxiosHeaders,
-    AxiosRequestConfig,
-    AxiosResponse,
-    InternalAxiosRequestConfig,
-  } from 'axios';
-  
-  import { API_URL } from '@/config';
-  import storage from '@/utils/storage';
-  
-  const addAuthorization = (config: AxiosRequestConfig, token: string): AxiosRequestConfig => {
-    const headers = config.headers as AxiosHeaders;
-    if (headers.get('Authorization')) return config;
-  
-    headers.set('Authorization', `Bearer ${token}`);
-    config.headers = headers;
-  
-    return config;
-  };
-  
-  const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
-    const token = storage.getAccessToken();
-    if (token) return addAuthorization(config, token);
-  
-    return config;
-  };
-  
-  const onRequestError = (error: AxiosError): Promise<AxiosError> => {
-    return Promise.reject(error);
-  };
-  const onResponse = (response: AxiosResponse): AxiosResponse => {
-    return response;
-  };
-  
-  type ResponseError = AxiosError & {
-    response: { data: { error: string; message: string; statusCode: number } };
-  };
-  
-  export type AxiosResponseError = AxiosError & { axiosMessage: string; message: string };
-  
-  const onResponseError = async (error: ResponseError): Promise<AxiosResponseError> => {
-    const config = error.config as AxiosRequestConfig & { sent?: boolean };
-  
-    if (error?.response?.status === 401 && !config.sent) {
-      config.sent = true;
-  
-      try {
-        const refreshToken = storage.getRefreshToken();
-        const response = await Axios.post(
-          API_URL + '/auth/jwt/refresh',
-          {},
-          { headers: { ['Authorization']: `Bearer ${refreshToken}` } }
-        );
-  
-        if (!response.data.accessToken) {
-          storage.clearTokens();
-        }
-  
-        storage.setTokens(response.data);
-        return axios(addAuthorization(config, response.data.accessToken));
-      } catch (error) {
+  AxiosError,
+  AxiosHeaders,
+  AxiosRequestConfig,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+
+import { API_URL } from '@/config';
+import storage from '@/utils/storage';
+
+const addAuthorization = (
+  config: AxiosRequestConfig,
+  token: string
+): AxiosRequestConfig => {
+  const headers = config.headers as AxiosHeaders;
+  if (headers.get('Authorization')) return config;
+
+  headers.set('Authorization', `Bearer ${token}`);
+  config.headers = headers;
+
+  return config;
+};
+
+const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+  const token = storage.getAccessToken();
+  if (token) return addAuthorization(config, token);
+
+  return config;
+};
+
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
+
+type ResponseError = AxiosError & {
+  response: { data: { error: string; message: string; statusCode: number } };
+};
+
+export type AxiosResponseError = AxiosError & {
+  axiosMessage: string;
+  message: string;
+};
+
+const onResponseError = async (
+  error: ResponseError
+): Promise<AxiosResponseError> => {
+  const config = error.config as AxiosRequestConfig & { sent?: boolean };
+
+  if (error?.response?.status === 401 && !config.sent) {
+    config.sent = true;
+
+    try {
+      const refreshToken = storage.getRefreshToken();
+      const response = await Axios.post(
+        API_URL + '/auth/jwt/refresh',
+        {},
+        { headers: { ['Authorization']: `Bearer ${refreshToken}` } }
+      );
+
+      if (!response.data.accessToken) {
         storage.clearTokens();
       }
+
+      storage.setTokens(response.data);
+      return axios(addAuthorization(config, response.data.accessToken));
+    } catch (error) {
+      storage.clearTokens();
     }
-  
-    return Promise.reject({
-      ...error,
-      axiosMessage: error.message,
-      message: error.response?.data?.message || 'Ooops, something went wrong',
-    });
-  };
-  
-  export const axios = Axios.create({
-    baseURL: API_URL,
+  }
+
+  return Promise.reject({
+    ...error,
+    axiosMessage: error.message,
+    message: error.response?.data?.message || 'Ooops, something went wrong',
   });
-  
-  axios.interceptors.request.use(
-    onRequest as (value: InternalAxiosRequestConfig<any>) => InternalAxiosRequestConfig<any>,
-    onRequestError
-  );
-  axios.interceptors.response.use(onResponse, onResponseError);
-  
+};
+
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
+
+axios.interceptors.request.use(
+  onRequest as (
+    value: InternalAxiosRequestConfig<any>
+  ) => InternalAxiosRequestConfig<any>,
+  onRequestError
+);
+axios.interceptors.response.use(onResponse, onResponseError);
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,17 +2,29 @@ const storagePrefix = 'app_';
 
 const storage = {
   getAccessToken: () => {
-    return JSON.parse(localStorage.getItem(`${storagePrefix}access_token`) as string);
+    return JSON.parse(
+      localStorage.getItem(`${storagePrefix}access_token`) as string
+    );
   },
   getRefreshToken: () => {
-    return JSON.parse(localStorage.getItem(`${storagePrefix}refresh_token`) as string);
+    return JSON.parse(
+      localStorage.getItem(`${storagePrefix}refresh_token`) as string
+    );
   },
   getWizardToken: () => {
-    return JSON.parse(localStorage.getItem(`${storagePrefix}wizard_access_token`) as string);
+    return JSON.parse(
+      localStorage.getItem(`${storagePrefix}wizard_access_token`) as string
+    );
   },
   setTokens: (tokens: { accessToken: string; refreshToken: string }) => {
-    localStorage.setItem(`${storagePrefix}access_token`, JSON.stringify(tokens.accessToken));
-    localStorage.setItem(`${storagePrefix}refresh_token`, JSON.stringify(tokens.refreshToken));
+    localStorage.setItem(
+      `${storagePrefix}access_token`,
+      JSON.stringify(tokens.accessToken)
+    );
+    localStorage.setItem(
+      `${storagePrefix}refresh_token`,
+      JSON.stringify(tokens.refreshToken)
+    );
   },
   clearTokens: () => {
     localStorage.removeItem(`${storagePrefix}access_token`);
@@ -20,7 +32,10 @@ const storage = {
     localStorage.removeItem(`${storagePrefix}wizard_access_token`);
   },
   setWizardToken: (token: string) => {
-    localStorage.setItem(`${storagePrefix}wizard_access_token`, JSON.stringify(token));
+    localStorage.setItem(
+      `${storagePrefix}wizard_access_token`,
+      JSON.stringify(token)
+    );
   },
   clearWizardToken: () => {
     localStorage.removeItem(`${storagePrefix}wizard_access_token`);
